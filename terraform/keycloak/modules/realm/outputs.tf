8330e91d8acf4da1c144ab905c141904b0108e28
Ilya Papou <ilya@papou.email>
2026-08-17 22:23:42 -0300

    fix(keycloak): scope provider realm admin role

--- terraform/keycloak/modules/realm/outputs.tf
+++ terraform/keycloak/modules/realm/outputs.tf
@@ -3,6 +3,7 @@ output "auth_broker_client_id" { value = keycloak_openid_client.auth_broker.clie
 output "terraform_client_ready" {
   value = (
     keycloak_openid_client_service_account_role.terraform_realm_admin.id != "" &&
-    keycloak_openid_client_default_scopes.terraform_provider.id != ""
+    keycloak_openid_client_default_scopes.terraform_provider.id != "" &&
+    keycloak_generic_role_mapper.terraform_realm_admin_scope.id != ""
   )
 }
